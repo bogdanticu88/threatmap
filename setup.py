08309ae59fe70ef7409b982e0d49af3208be2eef
Bogdan Ticu <bogdanticuoffice@gmail.com>
2026-02-26 21:58:46 +0200

    Bump version to v1.1.1 for PyPI publishing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="threatmap",
-    version="1.1.0",
+    version="1.1.1",
     description="Static IaC threat modeler using STRIDE",
     author="Bogdan Ticu",
     author_email="bogdanticuoffice@gmail.com",
